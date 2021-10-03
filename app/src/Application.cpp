034b3e022fac2d3abed8e2d7f837f0c815d839d1
Kopysov Evgeniy <SmileGobo@gmail.com>
2021-10-03 10:06:58 +0300

    Доработал приложение до чтения конфига из JSON файлов

--- app/src/Application.cpp
+++ app/src/Application.cpp
@@ -1,19 +1,34 @@
 #include "Application.h"
 #include <Poco/Util/Option.h>
-#include <TrasnsportType.h>
-
+#include "TrasnsportType.h"
+#include <Poco/JSON/Parser.h>
+#include "JSSONBuilder.h"
+#include "Factory.h"
+#include <fstream>
 Application::Application()
 {
 
 }
 
 void Application::initialize(Poco::Util::Application& app) {
+    logger().information("Name of config: %s", config().getString("config"));
+    std::ifstream file(config().getString("config"));
+    Poco::JSON::Parser parser;
+    auto config = parser.parse(file).extract<Poco::JSON::Object::Ptr>();
+    JSSONBuilder bldr(config->getObject("config"));
+    TransportType ttype;
+    ttype.set(config->getValue<std::string>("type"));
+    Factory fctry;
+    _instance = fctry.create(ttype.get(), 0);
+    if (_instance){
+        _instance->init(bldr);
+    }
 
 }
 
 void Application::defineOptions(Poco::Util::OptionSet& options) {
     Poco::Util::Option cfg("config", "c", "params file for instance");
-    cfg.required(true).argument("config");
+    cfg.required(true).argument("config").binding("config");
 
     options.addOption(cfg);
 }
